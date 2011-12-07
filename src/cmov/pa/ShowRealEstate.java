6eb1b3169e97308803822fee84e15e980f1ad136
Fernando Graças <fgracas89@gmail.com>
2011-12-07 18:25:05 +0000

    adicionada transicao panisgas entre atividades

--- src/cmov/pa/ShowRealEstate.java
+++ src/cmov/pa/ShowRealEstate.java
@@ -1,6 +1,7 @@
 package cmov.pa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.GestureDetector;
@@ -16,16 +17,12 @@ import android.widget.ViewFlipper;
 
 public class ShowRealEstate  extends Activity {
 	
-	 private static final int SWIPE_MIN_DISTANCE = 70;
+	 private static final int SWIPE_MIN_DISTANCE = 100;
 	 private static final int SWIPE_MAX_OFF_PATH = 150;
-	 private static final int SWIPE_THRESHOLD_VELOCITY = 100;
+	 private static final int SWIPE_THRESHOLD_VELOCITY = 150;
 	 private GestureDetector gestureDetector;
 	 View.OnTouchListener gestureListener;
-	 private Animation slideLeftIn;
-	 private Animation slideLeftOut;
-	 private Animation slideRightIn;
-	 private Animation slideRightOut;
-	 private ViewFlipper viewFlipper;
+	 
 	 int scroll = 0;
 	 private final int scrollDesta = 20;
 	 int width;
@@ -36,12 +33,6 @@ public class ShowRealEstate  extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_real_estate);
-        
-        viewFlipper = (ViewFlipper)findViewById(R.id.flipper);
-        slideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_left_in);
-        slideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_left_out);
-        slideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_right_in);
-        slideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_right_out);
         
         gestureDetector = new GestureDetector(new MyGestureDetector());
         gestureListener = new View.OnTouchListener() {
@@ -57,9 +48,9 @@ public class ShowRealEstate  extends Activity {
         width = display.getWidth();
         height = display.getHeight();
         
-        System.out.println(width + " " + height);
+        //System.out.println(width + " " + height);
         
-        ((RelativeLayout)findViewById(R.id.RelativeLayout1)).scrollTo(0, 0);
+        //((RelativeLayout)findViewById(R.id.RelativeLayout1)).scrollTo(0, 0);
 
     }
   	
@@ -72,7 +63,7 @@ public class ShowRealEstate  extends Activity {
         	RelativeLayout r = (RelativeLayout)findViewById(R.id.RelativeLayout1);
         	if(e1.getY() > e2.getY()){
         		scroll -= 20;
-        		System.out.println("scroll up");
+        		//System.out.println("scroll up");
         		if(scroll <= 0){
         			r.scrollTo(0, 0);
         			scroll = 0;
@@ -81,7 +72,7 @@ public class ShowRealEstate  extends Activity {
         		}
         	}else{
         		scroll+= 20;
-        		System.out.println("scroll down");
+        		//System.out.println("scroll down");
         		if(scroll >= Math.abs(r.getHeight()- height)){
         			r.scrollTo(0, Math.abs(r.getHeight()- height));
         			scroll = Math.abs(r.getHeight()- height);
@@ -92,11 +83,10 @@ public class ShowRealEstate  extends Activity {
         	}
         	
         	System.out.println(r.getHeight());
+        	System.out.println(r.getWidth());
         	//System.out.println("baseline-> " + r.getBaseline());
         	//System.out.println("bottom-> " +r.getBottom());
-        	
 
-        	
 			return super.onScroll(e1, e2, distanceX, distanceY);
 		}
 
@@ -107,18 +97,18 @@ public class ShowRealEstate  extends Activity {
                     return false;
                 // right to left swipe
                 if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                	System.out.println("right to left");
-                	viewFlipper.setInAnimation(slideLeftIn);
-                    //viewFlipper.setOutAnimation(slideLeftOut);
-                	viewFlipper.showNext();
-                	((RelativeLayout)findViewById(R.id.RelativeLayout1)).scrollTo(0, 0);
+                	System.out.println("right to left");                	
+                	Intent intent = new Intent(getApplicationContext(),ShowRealEstate.class);
+                    startActivity(intent);
+                    overridePendingTransition  (R.anim.right_slide_out, R.anim.right_slide_in);
+                    finish();
           
                 }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                	System.out.println("left to right");
-                	viewFlipper.setInAnimation(slideRightIn);
-                    //viewFlipper.setOutAnimation(slideRightOut);
-                	viewFlipper.showPrevious();
-                	((RelativeLayout)findViewById(R.id.RelativeLayout1)).scrollTo(0, 0);
+                	System.out.println("left to right");                	
+                	Intent intent = new Intent(getApplicationContext(),ShowRealEstate.class);
+                    startActivity(intent);
+                    overridePendingTransition  (R.anim.right_slide_in, R.anim.right_slide_out);
+                    finish();
                 }
             } catch (Exception e) {
                 // nothing
